27ab4ecf61dd28394951403fdac75e13a656808e
JaneDalton <jane@9thd.co.uk>
2017-03-04 14:04:43 +0000

    more BEM elements

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'); 
+var watch = require('gulp-watch');
+
+gulp.task('watch', function(){
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('styles'); 
+    });
+});
